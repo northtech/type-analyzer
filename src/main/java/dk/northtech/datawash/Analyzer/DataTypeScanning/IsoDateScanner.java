6a75099d513475745c24c1f4c9ccbaaee83a33c4
Jakob Kofod Beyer <jakob@hvalfisk.dk>
2018-03-23 10:39:26 +0100

    Add ISO date scanning from Jes' repository

--- src/main/java/dk/northtech/datawash/Analyzer/DataTypeScanning/IsoDateScanner.java
+++ src/main/java/dk/northtech/datawash/Analyzer/DataTypeScanning/IsoDateScanner.java
@@ -1,10 +1,13 @@
 package dk.northtech.datawash.Analyzer.DataTypeScanning;
 
+import dk.northtech.gsonextensions.javatypes.temporal.InstantTypeAdapterFactory;
+import dk.northtech.gsonextensions.javatypes.temporal.ZonedDateTimeTypeAdapterFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.time.Instant;
+import java.time.format.DateTimeParseException;
 
 @ParametersAreNonnullByDefault
 public class IsoDateScanner extends DataTypeScanner<Instant> {
@@ -13,6 +16,13 @@ public class IsoDateScanner extends DataTypeScanner<Instant> {
   
   @Override
   public boolean scan(Object value) {
-    return false;
+    try {
+      return (InstantTypeAdapterFactory.parse(value) != null) || (ZonedDateTimeTypeAdapterFactory.parse(value
+                                                                                                          .toString()) !=
+                                                                  null);
+    }
+    catch (DateTimeParseException e) {
+      return false;
+    }
   }
 }
