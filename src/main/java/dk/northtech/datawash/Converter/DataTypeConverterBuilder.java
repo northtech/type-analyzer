04f52ddafd1ee4fd0ca40c49f53b27d81c5f9690
Jakob Kofod Beyer <jakob@hvalfisk.dk>
2018-04-05 11:39:13 +0200

    Add new package for the converter with empty files

---
+++ src/main/java/dk/northtech/datawash/Converter/DataTypeConverterBuilder.java
@@ -0,0 +1,11 @@
+package dk.northtech.datawash.Converter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@ParametersAreNonnullByDefault
+public class DataTypeConverterBuilder {
+  private static final Logger LOGGER = LoggerFactory.getLogger(DataTypeConverterBuilder.class);
+}
